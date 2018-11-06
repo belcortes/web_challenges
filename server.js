9e2693580e789e431a9a397e19432eda458cbb00
belcortes <belcortes@gmail.com>
2018-11-06 15:49:05 -0800

    fetching by id, added state

--- server.js
+++ server.js
@@ -1,44 +1,42 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient
 const bodyParser= require('body-parser')
+
 const app = express();
+const port = process.env.PORT || 4000;
 
 app.use(bodyParser.urlencoded({extended: true}))
+ObjectId = require('mongodb').ObjectID
 
-
-MongoClient.connect('mongodb://user1:webchallenges1@ds153093.mlab.com:53093/web-challenges',{ useNewUrlParser: true },function(err,client){
+MongoClient.connect('mongodb://user1:webchallenges1@ds153093.mlab.com:53093/web-challenges', { useNewUrlParser: true }, (err,client) => {
   if (err) return console.log(err)
-  console.log('made it!')
-  db = client.db('web-challenges') // whatever your database name is
-  app.listen(3000, () => {
-    console.log('listening on 3000')
-  })
+
+  db = client.db('web-challenges')
+  app.listen(port, () => console.log('listening on 4000'));
 
   app.post('/challenges', (req, res) => {
     db.collection('challenges').insertOne(req.body, (err, result) => {
       if (err) return console.log(err)
 
       console.log('saved to database')
-      // res.redirect('/')
     })
   })
 
-  app.get('/', (req, res) => {
-    console.log('yo!')
-    db.collection('challenges').find().toArray(function(err, results) {
-      console.log(results)
-      // send HTML file populated with quotes here
+  app.get('/challenges', (req, res) => {
+    db.collection('challenges').find().toArray((err, results) => {
+      if (err) return console.log(err)
+
+      res.send(results)
     })
-    // console.log(cursor)
+  })
+
+  app.get('/challenge', (req, res) => {
+    db.collection('challenges').findOne({_id: new ObjectId(req.query.id)}, (err, challenge) => {
+      if (err) return console.log(err)
+
+      res.send(challenge)
+    })
   })
 
 
 })
-
-
-
-// app.get('/',function(req,res) {
-//   res.send('Hello World!');
-// });
-
-let server=app.listen(4000,() => console.log('listening on 4000'));
